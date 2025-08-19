5b3f0608c5cb8cc10d9380a68f143e363d87c4a3
Lowkeyprashant <lowkeyprashant@gmail.com>
2025-08-19 23:12:07 +0530

    Changes

--- app/src/main/java/com/codewithprashant/mindly/MainActivity.kt
+++ app/src/main/java/com/codewithprashant/mindly/MainActivity.kt
@@ -9,11 +9,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.codewithprashant.mindly.ui.theme.MindlyTheme
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.core.view.WindowCompat
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, true)
         setContent {
             MindlyTheme {
                 Surface(
